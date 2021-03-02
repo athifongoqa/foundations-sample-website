6ea17325e1a14b4d35c7a0932bd565fbf9298f0b
Athi Fongoqa <athi.fongoqa@code.berlin>
2021-03-02 21:11:52 +0200

    Week One

--- week1/website.py
+++ week1/website.py
@@ -4,11 +4,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/first-page')
 def first_page():
     return render_template('first-page.html', page_title="First Template")
 
@@ -18,6 +13,11 @@ def second_page():
     return render_template('second-page.html', page_title="Second Template")
 
 # add additonal pages here using a similar format as above
+
+
+@app.route('/third-page')
+def third_page():
+    return render_template('third-page.html', page_title="Third Template")
 
 
 if __name__ == "__main__":
