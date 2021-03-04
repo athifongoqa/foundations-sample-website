b722ab3330dc01c48fcac6d50218b226f222a860
Athi Fongoqa <athi.fongoqa@code.berlin>
2021-03-04 23:42:46 +0200

    Damage Control

--- sample_website/tests/test_sample_website.py
+++ sample_website/tests/test_sample_website.py
@@ -3,8 +3,5 @@ from sample_website.website import hello_world
 
 def test_sample_website():
 
-    # << << << < HEAD
     assert hello_world() == '<h1>Hello, Everyone!</h1>'
-    # == == == =
     assert hello_world() == '<h1>Hello, Class!</h1>'
-    # >>>>>> > upstream/main
